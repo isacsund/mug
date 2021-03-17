828a648e4daf98a1f454544c52d4b7f3ed25debe
Isac Sund <isac@isacsund.com>
2021-03-17 18:08:52 +0200

    src/cmd/search: search for package in aur
    Search for the package in AUR[0].
    
    This functionality uses raur[1] for sending requests to the AUR RPC. The
    search uses the default searching method, which is by package name and
    description. The returned matches are printed to the stdout.
    
    [0] https://aur.archlinux.org
    [1] https://gitlab.com/davidbittner/raur

--- src/cmd/search.rs
+++ src/cmd/search.rs
@@ -1,5 +1,6 @@
 // 3rd party imports {{{
 use clap::Clap;
+use raur::Raur;
 // }}}
 
 #[derive(Clap)]
@@ -8,5 +9,23 @@ pub struct CliArgs {
 }
 
 pub async fn handler(args: CliArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let raur = raur::Handle::new();
+
+    let packages = raur.search(args.package).await?;
+
+    for package in packages {
+        let stats = format!("+{} ~{:.2}", package.num_votes, package.popularity);
+
+        print!(
+            "{}/{} {} [{}]",
+            "aur",
+            &package.name,
+            &package.version,
+            stats,
+        );
+
+        print!("\n");
+    }
+
     Ok(())
 }
