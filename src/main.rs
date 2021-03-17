496ca04c35f4433d2b10388b7fe80eec2d029803
Isac Sund <isac@isacsund.com>
2021-03-17 15:56:27 +0200

    src/main: add basic command line parser
    Add a basic command line parser using clap[0].
    
    The main idea is to implement subcommands in `src/cmd/<subcommand>.rs`
    with their own handlers. The subcommands will be added to the `Subcommand`
    enum and matched on with the parsed arguments.
    
    [0] https://github.com/clap-rs/clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// 3rd party imports {{{
+use clap::{
+    Clap, 
+    crate_authors,
+    crate_version,
+};
+// }}}
+
+#[derive(Clap)]
+enum SubCommand {
+}
+
+#[derive(Clap)]
+#[clap(
+    author = crate_authors!(),
+    version = crate_version!(),
+)]
+struct CliArgs {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = CliArgs::parse();
 }
