7be4760b5a591785bc4c0be07e1d25efbea073fd
Isac Sund <isac@isacsund.com>
2021-03-17 20:44:18 +0200

    src/cmd: introduce info subcommand
    Introduce an `info` subcommand.
    
    This command will fetch information about the package from AUR (given
    that it exists), and print it to stdout. There's a lot of improvements
    to make when displaying the information: terminal colors, default value
    for an empty field and handle packages that aren't found, to name a few.

---
+++ src/cmd/info.rs
@@ -0,0 +1,50 @@
+// 3rd party imports {{{
+use clap::Clap;
+use raur::Raur;
+// }}}
+
+// Pretty print lists
+macro_rules! printlist {
+    ($h:expr, $v:expr) => {{
+        println!("{}", $h);
+        for v in $v {
+            println!("{:indent$}{}", "", v, indent=2)
+        }
+    }};
+}
+
+#[derive(Clap)]
+pub struct CliArgs {
+    packages: Vec<String>,
+}
+
+pub async fn handler(args: CliArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let raur = raur::Handle::new();
+
+    // TODO: check if all packages were found
+    // raur.info doesn't return any errors if the package isn't found
+    let packages = raur.info(&args.packages).await?;
+
+    for package in packages {
+        println!("Repository: aur");
+        println!("Name: {}", &package.name);
+        println!("Version: {}", &package.version);
+        println!("Description: {}", &package.description.unwrap_or("".to_string()));
+        println!("URL: {}", &package.url.unwrap_or("".to_string()));
+        println!("Maintainer: {}", &package.maintainer.unwrap_or("".to_string()));
+        printlist!("Groups:", &package.groups);
+        printlist!("Licenses:", &package.license);
+        printlist!("Provides:", &package.provides);
+        printlist!("Depends on:", &package.depends);
+        printlist!("Make dependencies:", &package.make_depends);
+        printlist!("Check dependencies:", &package.check_depends);
+        printlist!("Optional dependencies:", &package.opt_depends);
+        printlist!("Conflicts with:", &package.conflicts);
+        println!("Votes: {}", &package.num_votes.to_string());
+        println!("Popularity: {}", &package.popularity.to_string());
+
+        println!();
+    }
+
+    Ok(())
+}
