8d2cebf94d0f88ef100da6f842b8156daf1127be
Isac Sund <isac@isacsund.com>
2021-04-21 22:40:50 +0300

    src/cmd/list: add unofficial flag
    Add an `--unofficial` flag which will remove packages coming from the
    official repositores from the list of packages to be printed.

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -9,13 +9,22 @@ use crate::error::Error;
 // }}}
 
 #[derive(Clap)]
-pub struct CliArgs {}
+pub struct CliArgs {
+    /// List only packages installed from unofficial repositories.
+    #[clap(short, long)]
+    unofficial: bool,
+}
 
-pub async fn handler(_args: CliArgs) -> Result<(), Error> {
+pub async fn handler(args: CliArgs) -> Result<(), Error> {
     let alpm = alpm::Handle::new()?;
 
     let db = alpm.client().localdb();
-    let packages = db.pkgs().iter().collect::<Vec<_>>();
+    let mut packages = db.pkgs().iter().collect::<Vec<_>>();
+
+    // Filter out official packages if the `--unofficial` flag is present.
+    if args.unofficial {
+        packages.retain(|p| !alpm.is_official_package(p));
+    }
 
     // TODO: pretty print with a header e.g.
     // Package Version
